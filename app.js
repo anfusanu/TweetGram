e3a03c0f8eea510e3f6d6d3e26264b4369a6f19e
anfusanu <anfusanu@gmail.com>
2021-06-20 05:41:01 +0530

    static serve

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static('public'));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   secret: 'keyboard_cat',
   cookie: { maxAge: 6000000 },
