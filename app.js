71f2a4ca2785df4881b6cafa2ad7be2a57bb1eb8
anfusanu <anfusanu@gmail.com>
2021-06-23 19:23:49 +0530

    last touch

--- app.js
+++ app.js
@@ -81,8 +81,8 @@ app.use(passport.initialize());
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  // callbackURL: "https://nacmen.anfus.xyz/google"
-  callbackURL: "http://localhost:3001/google"
+  callbackURL: "https://nacmen.anfus.xyz/google"
+  // callbackURL: "http://localhost:3001/google"
 
 
 },
